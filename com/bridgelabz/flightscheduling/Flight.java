05bce4ce5347aeecb853e6fb55612759971ed122
pranav1034 <pranavaggarwal04@gmail.com>
2025-03-27 11:09:31 +0530

    [Pranav] ADD developed-flight-model

---
+++ com/bridgelabz/flightscheduling/Flight.java
@@ -0,0 +1,29 @@
+package com.bridgelabz.flightscheduling;
+
+public class Flight<T> {
+    private T flightNumber;
+    private String departure;
+    private String destination;
+
+    public Flight(T flightNumber, String departure, String destination) {
+        this.flightNumber = flightNumber;
+        this.departure = departure;
+        this.destination = destination;
+    }
+
+    public T getFlightNumber(){
+        return flightNumber;
+    }
+
+    public String getDeparture(){
+        return departure;
+    }
+
+    public String getArrival(){
+        return destination;
+    }
+    public void displayFlightInfo() {
+        System.out.println("Flight: " + flightNumber + " | From: " + departure + " | To: " + destination);
+    }
+
+}
