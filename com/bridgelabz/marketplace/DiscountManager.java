7b7d782c59e5084073efd465745857db15744cf6
pranav1034 <pranavaggarwal04@gmail.com>
2025-03-27 15:35:55 +0530

    [Pranav] ADD created-discount-class-to-apply-discount-to-products

--- com/bridgelabz/marketplace/DiscountManager.java
+++ com/bridgelabz/marketplace/DiscountManager.java
@@ -3,7 +3,7 @@ package com.bridgelabz.marketplace;
 public class DiscountManager {
     public static <T extends ProductCategory> void applyDiscount(Product<T> product, double percentage){
         double price = product.getPrice();
-        double newprice = price - (price * percentage / 100);
-        product.setPrice(newprice);
+        price = price - (price * percentage / 100);
+        product.setPrice(price);
     }
 }
