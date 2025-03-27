f67fd2c1c846bf2a913d83da29a0e11a3ca91d67
pranav1034 <pranavaggarwal04@gmail.com>
2025-03-27 14:19:05 +0530

    [Pranav] ADD created-storage-class-to-store-different-items

--- com/bridgelabz/smartwarehouse/Storage.java
+++ com/bridgelabz/smartwarehouse/Storage.java
@@ -1,11 +1,15 @@
 package com.bridgelabz.smartwarehouse;
-import java.util.ArrayList;
+import java.util.*;
 
 public class Storage<T extends WarehouseItem> {
     private ArrayList<T> items = new ArrayList<>();
 
     public void addItem(T item) {
         items.add(item);
+    }
+
+    public List<T> getItems() {
+        return items;
     }
 
 }
